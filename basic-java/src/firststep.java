82cb5a2d5ef974fed240fc41f7f6c88c7a559492
Pipoune974 <130435869+Pipoune974@users.noreply.github.com>
2025-09-04 01:50:30 +0400

    up

--- basic-java/src/firststep.java
+++ basic-java/src/firststep.java
@@ -1,5 +1,16 @@
 public class firststep {
-     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    enum Mois{
+        JANVIER, FEVRIER, MARS, AVRIL, MAI, JUIN, JUILLET, AOUT, SEPTEMBRE, OCTOBRE, NOVEMBRE, DECEMBRE
     }
-}
+     public static void main(String[] args) {
+
+        Mois m = Mois.valueOf("JANVIER");
+        System.out.println(m);
+
+        int[]tableauAffaire ;
+        tableauAffaire = new int[]{1,2,3,4,5,6,7,8,9,10};
+        tableauAffaire[9] = 1;
+
+        System.out.println(tableauAffaire[9]);
+        }
+    }
